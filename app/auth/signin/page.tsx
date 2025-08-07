f5fe76966e6b5f3dba4b17553938bfb388bbac1e
Mohamad Alkaadi <mohamad-akaadi>
2025-08-07 16:17:15 +0400

    first commit

---
+++ app/auth/signin/page.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const page = () => {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <div>signin</div>
+    </div>
+  )
+}
+
+export default page
