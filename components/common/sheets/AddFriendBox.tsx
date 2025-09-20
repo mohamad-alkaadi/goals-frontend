e19bf92d2de4854911c4343f58fd4ff180d474f3
mohamad-alkaadi <mohamadalkaadi@gmail.com>
2025-09-20 16:35:02 +0400

    friends v2

--- components/common/sheets/AddFriendBox.tsx
+++ components/common/sheets/AddFriendBox.tsx
@@ -1,12 +1,15 @@
-import { addFriend } from "@/api/friends";
-import React, { useState } from "react";
+import { addFriend, SearchResult } from "@/api/friends";
+import React, { useRef, useState } from "react";
 import { IoIosSearch } from "react-icons/io";
 import { IoAdd } from "react-icons/io5";
 
 const AddFriendBox = () => {
   const [submitActive, setSubmitActive] = useState(false);
   const [email, setEmail] = useState("");
-  const [searchResult, setSearchResult] = useState("");
+  const [searchResult, setSearchResult] = useState<SearchResult>({
+    resSuccess: false,
+    message: "",
+  });
   return (
     <div className="flex flex-col justify-center items-start">
       <form
@@ -16,6 +19,7 @@ const AddFriendBox = () => {
           setSubmitActive(true);
           addFriend(email, setSearchResult);
           setEmail("");
+          setSubmitActive(false);
         }}
         className="flex justify-center items-center w-full border-[1px] border-[#9b9b9b] space-x-1"
       >
@@ -36,11 +40,18 @@ const AddFriendBox = () => {
           className={`cursor-pointer hover:bg-[#373737] h-full py-2 px-2 ${
             submitActive && "cursor-wait"
           }`}
+          disabled={email == "" || submitActive}
         >
           <IoAdd />
         </button>
       </form>
-      <div className="pl-1">{searchResult}</div>
+      <div
+        className={`pl-1 ${
+          searchResult.resSuccess ? "text-green-500" : "text-red-500"
+        }`}
+      >
+        {searchResult.message}
+      </div>
     </div>
   );
 };
