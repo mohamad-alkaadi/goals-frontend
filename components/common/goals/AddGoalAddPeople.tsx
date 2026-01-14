21e9fe91d9e4521370ee21af77a030ecac4b24d1
mohamad-alkaadi <mohamadalkaadi@gmail.com>
2026-01-14 01:53:37 +0100

    share goal v1

--- components/common/goals/AddGoalAddPeople.tsx
+++ components/common/goals/AddGoalAddPeople.tsx
@@ -5,12 +5,13 @@ import {
   TooltipContent,
   TooltipTrigger,
 } from "@/components/ui/tooltip"
-
+import AddGoalAddPeopleDialog from "./AddGoalAddPeopleDialog"
 const AddGoalAddPeople = () => {
   return (
     <Tooltip>
-      <TooltipTrigger className="hover:bg-[#474747] h-[47px] w-[32px] flex justify-center items-center cursor-pointer">
-        <GoPeople />
+      <TooltipTrigger asChild className="hover:bg-[#474747] h-[47px] w-[32px] flex justify-center items-center cursor-pointer">
+        {/* <GoPeople />*/}
+        <AddGoalAddPeopleDialog />
       </TooltipTrigger>
       <TooltipContent className="bg-[#484848] rounded-sm">
         <p>Add people</p>
--- components/common/goals/AddGoalAddPeopleDialog.tsx
+++ components/common/goals/AddGoalAddPeopleDialog.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import {
   AlertDialog,
   AlertDialogAction,
@@ -11,18 +11,44 @@ import {
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog"
 import { GoPeople } from "react-icons/go"
+import { FriendsApiResponse, FriendsType, getAllFriends } from "@/api/friends";
 const AddGoalAddPeopleDialog = () => {
+  const [friends, setFriends] = useState<FriendsType[]>([]);
+  const [activeFriends, setActiveFriends] = useState<String[]>([])
+  useEffect(() => {
+    const getData = async () => {
+      const data: FriendsApiResponse = await getAllFriends();
+      setFriends(data.friends);
+    };
+    getData();
+  }, []);
+  useEffect(() => {
+    console.log("friends yoyo: ", friends)
+  }, [friends])
+
+  const handleFriendClick = (friendId: string) => {
+    setActiveFriends((prev) => {
+      if (prev.includes(friendId)) {
+        return prev.filter((id) => id !== friendId);
+      } else {
+        return [...prev, friendId];
+      }
+    });
+  };
   return (
     <AlertDialog>
-      <AlertDialogTrigger>
+      <AlertDialogTrigger className={`hover:bg-[#474747] h-[47px] w-[32px] flex justify-center items-center cursor-pointer rounded-md`} >
         <GoPeople />
       </AlertDialogTrigger>
-      <AlertDialogContent>
+      <AlertDialogContent className="bg-[#1c1c1c] border-0 text-white h-[300px] flex flex-col">
         <AlertDialogHeader>
-          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-          <AlertDialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
+          <AlertDialogTitle>Add People</AlertDialogTitle>
+          <AlertDialogDescription className="h-[170px] overflow-auto flex flex-col space-y-1">
+            {friends.map(item => (
+              <button key={item._id} onClick={() => handleFriendClick(item._id)} className={`w-full cursor-pointer ${activeFriends.includes(item._id) ? "bg-[#373737]" : "bg-[#2a2a2a]"} h-[40px] flex items-center text-[17px] text-white capitalize pl-2 rounded-sm shrink-0`}>
+                {item.name}
+              </button>
+            ))}
           </AlertDialogDescription>
         </AlertDialogHeader>
         <AlertDialogFooter>
